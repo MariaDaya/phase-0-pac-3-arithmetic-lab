60f9435fa8d1718453958b84fba0691e153bf2f8
Maria Dayekh <mariaaldayekh@gmail.com>
2022-10-31 13:05:17 +0200

    Completed assignment

---
+++ index.js
@@ -0,0 +1,50 @@
+number =5;
+function add(){
+  return (number +=5);
+}
+
+function subtract(){
+    return (number-=3);
+}
+
+function multiply(){
+    return (number *=2);
+}
+
+function divide(){
+    return (number/=4);
+}
+
+function add (a,b){
+    return (a+b);
+}
+
+function subtract (a,b){
+    return(a-b);
+}
+
+function multiply(a,b){
+    return (a*b);
+}
+
+function divide (a,b){
+    return (a/b);
+}
+
+function increment (n){
+    n++;
+    return(n);
+}
+
+function decrement (n){
+    n--;
+    return (n);
+} 
+
+function makeInt(n){
+    return parseInt(n, 10);
+}
+
+function preserveDecimal (n){
+   return parseFloat(n);
+}
